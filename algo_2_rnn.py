ba6400279a6614f72757d9da34037ae52fa19726
Jeane-31 <112557151+Jeane-31@users.noreply.github.com>
2023-03-05 08:51:33 +0530

    Update algo_2_rnn.py

--- algo_2_rnn.py
+++ algo_2_rnn.py
@@ -5,49 +5,49 @@ from keras.layers import Dense
 from keras.models import Sequential
 from matplotlib import pyplot
 
-TRAINING_PERCENTAGE = 0.83
-TESTING_PERCENTAGE = 1 - TRAINING_PERCENTAGE
-NUMBER_OF_PREVIOUS_DATA_POINTS = 3
-LENGTH_DATA_SET = 0
-TRAINING_SET_LENGTH = 0
-TESTING_SET_LENGTH = 0
+train_ratio = 0.83
+test_ratio = 1 - train_ratio
+prev_data_points_numbers = 3
+data_set_len = 0
+set_train_len = 0
+set_test_len = 0
 
 
 def load_data():
     data_set_frame = read_csv('usdinr_d_2.csv', header=0, squeeze=True)
     column_headers = data_set_frame.columns.values.tolist()
-    currency_index = column_headers.index('Close')
+    col_idx = column_headers.index('Close')
 
-    data_file = read_csv("usdinr_d_2.csv", usecols=[currency_index], engine='python')
+    data_file = read_csv("usdinr_d_2.csv", usecols=[col_idx], engine='python')
     load_data = []
     for data_point in data_file.values.tolist():
         load_data.append(data_point[0])
-    global LENGTH_DATA_SET
-    LENGTH_DATA_SET = len(load_data)
+    global data_set_len
+    data_set_len = len(load_data)
     return load_data
 
 
-def split_train_test(load_data, training_percentage, testing_percentage):
-    global TRAINING_SET_LENGTH, TESTING_SET_LENGTH
-    TRAINING_SET_LENGTH = int(LENGTH_DATA_SET * training_percentage)
-    TESTING_SET_LENGTH = LENGTH_DATA_SET - TRAINING_SET_LENGTH
-    training_set, testing_set = load_data[0:TRAINING_SET_LENGTH], load_data[TRAINING_SET_LENGTH:LENGTH_DATA_SET]
+def split_train_test(load_data, train_ratio, test_ratio):
+    global set_train_len, set_test_len
+    set_train_len = int(data_set_len * train_ratio)
+    set_test_len = data_set_len - set_train_len
+    training_set, testing_set = load_data[0:set_train_len], load_data[set_train_len:data_set_len]
     return training_set, testing_set
 
 
 def modify_data_set_rnn(training_set, testing_set):
     train_actual = []
     train_predict = []
-    for interval in range(len(training_set) - NUMBER_OF_PREVIOUS_DATA_POINTS - 1):
-        train_actual.append(training_set[interval: interval + NUMBER_OF_PREVIOUS_DATA_POINTS])
-        train_predict.append(training_set[interval + NUMBER_OF_PREVIOUS_DATA_POINTS])
+    for interval in range(len(training_set) - prev_data_points_numbers - 1):
+        train_actual.append(training_set[interval: interval + prev_data_points_numbers])
+        train_predict.append(training_set[interval + prev_data_points_numbers])
 
     test_actual = []
     test_predict = []
 
-    for interval in range(len(testing_set) - NUMBER_OF_PREVIOUS_DATA_POINTS - 1):
-        test_actual.append(testing_set[interval: interval + NUMBER_OF_PREVIOUS_DATA_POINTS])
-        test_predict.append(testing_set[interval + NUMBER_OF_PREVIOUS_DATA_POINTS])
+    for interval in range(len(testing_set) - prev_data_points_numbers - 1):
+        test_actual.append(testing_set[interval: interval + prev_data_points_numbers])
+        test_predict.append(testing_set[interval + prev_data_points_numbers])
 
     return train_actual, train_predict, test_actual, test_predict
 
@@ -55,7 +55,7 @@ def modify_data_set_rnn(training_set, testing_set):
 def build_rnn(train_actual, train_predict):
     recurrent_neural_network = Sequential()
 
-    recurrent_neural_network.add(Dense(12, input_dim=NUMBER_OF_PREVIOUS_DATA_POINTS, activation="relu"))
+    recurrent_neural_network.add(Dense(12, input_dim=prev_data_points_numbers, activation="relu"))
     recurrent_neural_network.add(Dense(8, activation="relu"))
     recurrent_neural_network.add(Dense(1))
 
@@ -80,13 +80,13 @@ def evaluate_performance_rnn(recurrent_neural_network, test_actual, test_predict
 
 
 def rnn_graph_plot(load_data, training_predict, testing_predict, file_name):
-    training_data_trend = [None] * LENGTH_DATA_SET
-    testing_data_trend = [None] * LENGTH_DATA_SET
+    training_data_trend = [None] * data_set_len
+    testing_data_trend = [None] * data_set_len
 
-    training_data_trend[NUMBER_OF_PREVIOUS_DATA_POINTS:len(training_predict) + NUMBER_OF_PREVIOUS_DATA_POINTS] = list(training_predict[:, 0])
-    testing_data_trend[NUMBER_OF_PREVIOUS_DATA_POINTS - 1:len(training_predict) + NUMBER_OF_PREVIOUS_DATA_POINTS] = list(testing_predict[:, 0])
+    training_data_trend[prev_data_points_numbers:len(training_predict) + prev_data_points_numbers] = list(training_predict[:, 0])
+    testing_data_trend[prev_data_points_numbers - 1:len(training_predict) + prev_data_points_numbers] = list(testing_predict[:, 0])
 
-    actual = pyplot.plot(load_data[int(TRAINING_PERCENTAGE * LENGTH_DATA_SET):], label="Actual data points", color="blue")
+    actual = pyplot.plot(load_data[int(train_ratio * data_set_len):], label="Actual data points", color="blue")
     testing = pyplot.plot(testing_data_trend, label="Testing prediction", color="red")
 
     pyplot.ylabel('Currency Values for 1 USD')
@@ -103,7 +103,7 @@ def rnn_model():
 
     data = load_data()
 
-    training_set, testing_set = split_train_test(data, TRAINING_PERCENTAGE, TESTING_PERCENTAGE)
+    training_set, testing_set = split_train_test(data, train_ratio, test_ratio)
     train_actual, train_predict, test_actual, test_predict = modify_data_set_rnn(training_set, testing_set)
 
     rnn = build_rnn(train_actual, train_predict)
